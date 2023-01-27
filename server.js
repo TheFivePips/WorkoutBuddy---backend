6470a15d5a30b2d0f244ac6f7177782f7b8176e7
Sam Gardner <samgardner20@gmail.com>
2023-01-27 16:29:14 -0500

    removed the old front end connection

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, "./frontend/build")))
 
 // serving the frontend
-app.get("*", function ( req, res) {
-  res.sendFile(
-    path.join(__dirname, "./frontend/build/index.html"),
-    function(err) {
-      res.status(500).send(err)
-    }
-  )
-})
+// app.get("*", function ( req, res) {
+//   res.sendFile(
+//     path.join(__dirname, "./frontend/build/index.html"),
+//     function(err) {
+//       res.status(500).send(err)
+//     }
+//   )
+// })
 
 
 
